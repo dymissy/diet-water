0fc699283befe853a484e18a142bb7f753016b01
Simone D'Amico <simone.damico@spreaker.com>
2021-04-25 18:23:08 +0200

    Add ReviewSourcesRepository

---
+++ app/tests/Unit/Repository/ReviewSourcesRepositoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests\Unit\Repository;
+
+use App\Entity\Review;
+use App\Repository\ReviewSourcesRepository;
+use PHPUnit\Framework\TestCase;
+
+class ReviewSourcesRepositoryTest extends TestCase
+{
+    private ReviewSourcesRepository $repository;
+
+    protected function setUp(): void
+    {
+        $this->repository = new ReviewSourcesRepository('https://www.amazon.it/product');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnAllSources(): void
+    {
+        $this->assertEquals([Review::SOURCE_AMAZON => 'https://www.amazon.it/product'], $this->repository->findAll());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnSpecificSourc(): void
+    {
+        $this->assertEquals('https://www.amazon.it/product', $this->repository->find(Review::SOURCE_AMAZON));
+    }
+}
